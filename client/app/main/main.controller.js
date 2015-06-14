891df59f802ec184086e4758f4ba9af5d5d7136f
Patrick San Juan <pat.sanjuan@gmail.com>
2015-06-14 14:03:09 -0700

    drafted main page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,6 +1,15 @@
 'use strict';
 
 angular.module('bookClubApp')
-  .controller('MainCtrl', function ($scope, $window, $http) {
-      
+  .controller('MainCtrl', function ($scope, $window, $http, Auth) {
+
+    $scope.isCollapsed = true;
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+    $scope.getCurrentUser = Auth.getCurrentUser;
+
+    $scope.logout = function() {
+      Auth.logout();
+      $location.path('/login');
+    };
   });
