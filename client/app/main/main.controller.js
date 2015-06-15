2cb36f2f6d5084762045092d248a34c86eb323eb
Patrick San Juan <pat.sanjuan@gmail.com>
2015-06-15 15:20:15 -0700

    fixed redirect. drafted book section

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -11,14 +11,14 @@ angular.module('bookClubApp')
     $scope.errors = {};
 
     $scope.books = [
-      {name: 'bookname', desc:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita rem commodi voluptatem ut facilis enim reiciendis labore ratione voluptas fuga dolores voluptates error.'},
-      {name: 'bookname', desc:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque debitis, sequi enim, vel magnam possimus et fugit asperiores neque dolores, ipsa.'},
-      {name: 'bookname', desc:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, voluptates. Placeat obcaecati quam vel dolor fugiat blanditiis! Porro labore nostrum eum unde? Numquam solut.'},
-      {name: 'bookname', desc:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates eius distinctio nisi quae. Amet, repudiandae adipisci mollitia voluptates delectus aliquid, nesciunt perspiciatis optio accusantiu.'},
-      {name: 'bookname', desc:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae ipsam adipisci velit, laudantium quis! Doloribus molestias animi sed totam modi ipsum explicabo.'},
-      {name: 'bookname', desc:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores provident, accusamus possimus error! Quia nam, itaque velit ex voluptatem, tenetur sequ.'},
-      {name: 'bookname', desc:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod vitae, itaque at numquam laboriosam labore amet repellat vel consequatur dicta harum qu.'},
-      {name: 'bookname', desc:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero non obcaecati, mollitia voluptas accusamus, odit dicta ex id nisi error necessitatibu..'}
+      {name: 'bookname1', desc:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita rem commodi voluptatem ut facilis enim reiciendis labore ratione voluptas fuga dolores voluptates error.'},
+      {name: 'bookname2', desc:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque debitis, sequi enim, vel magnam possimus et fugit asperiores neque dolores, ipsa.'},
+      {name: 'bookname3', desc:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, voluptates. Placeat obcaecati quam vel dolor fugiat blanditiis! Porro labore nostrum eum unde? Numquam solut.'},
+      {name: 'bookname4', desc:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates eius distinctio nisi quae. Amet, repudiandae adipisci mollitia voluptates delectus aliquid, nesciunt perspiciatis optio accusantiu.'},
+      {name: 'bookname5', desc:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae ipsam adipisci velit, laudantium quis! Doloribus molestias animi sed totam modi ipsum explicabo.'},
+      {name: 'bookname6', desc:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores provident, accusamus possimus error! Quia nam, itaque velit ex voluptatem, tenetur sequ.'},
+      {name: 'bookname7', desc:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod vitae, itaque at numquam laboriosam labore amet repellat vel consequatur dicta harum qu.'},
+      {name: 'bookname8', desc:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero non obcaecati, mollitia voluptas accusamus, odit dicta ex id nisi error necessitatibu..'}
     ]
 
     $scope.login = function(form) {
